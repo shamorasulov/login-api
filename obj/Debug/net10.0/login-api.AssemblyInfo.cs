5c44af18cc92a0e7d6ae06ed9a7d9f50866f2114
Shamo Rasulov <shamorasulov@gmail.com>
2026-02-20 21:35:20 +0400

    fix

--- obj/Debug/net10.0/login-api.AssemblyInfo.cs
+++ obj/Debug/net10.0/login-api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("login-api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c82afa44241cef0277c1ab23b1515257124b668b")]
 [assembly: System.Reflection.AssemblyProductAttribute("login-api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("login-api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
